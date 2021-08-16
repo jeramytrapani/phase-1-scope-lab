68efd20c88989df37bd5b5eb97658c0b0381fa0d
jeramytrapani <jeramytrapani@gmail.com>
2021-08-16 15:48:04 -0700

    First Save

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+    //return customerName.toUpperCase();
+}
+
+setBestCustomer = () => {
+    bestCustomer = 'not bob';
+}
+
+overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob';
+    
+}
+
+const leastFavoriteCustomer = 'Brian';
+
+changeLeastFavoriteCustomer = () => {
+    //const leastFavoriteCustomer = 'John';
+} 
+
